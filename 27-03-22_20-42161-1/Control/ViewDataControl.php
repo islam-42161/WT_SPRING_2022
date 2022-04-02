bc39fb7513c7b85fd9a081484df1342d5c72b4d1
islam-42161 <88014973+islam-42161@users.noreply.github.com>
2022-04-02 08:12:38 +0600

    create and read: 👍🏿

--- 27-03-22_20-42161-1/Control/ViewDataControl.php
+++ 27-03-22_20-42161-1/Control/ViewDataControl.php
@@ -5,27 +5,41 @@ include ("../Model/Model.php");
 
 if(isset($_POST["seedata"]))
 {
-    echo "button clicked!";
         $dbobj = new db();
-        $conobj = $dbobj -> opencon();
-        $fetchedData = $dbobj -> fetchRow("person",$conobj);
+        $conobj = $dbobj->opencon();
+        $fetchedData = $dbobj->fetchRow($conobj,"person");
 
-        if($fetchedData -> num_rows>0){
-            while($row=$fetchedData->fetch_assoc()){
-               
-                echo $row["fname"]."</br>";
-                echo $row["lname"]."</br>";
-                echo $row["age"]."</br>";
-                echo $row["salary"]."</br>";
-                echo $row["address"]."</br>";
+        if( $fetchedData !==false && $fetchedData->num_rows>0)
+        {
+            echo("<h3>User data</h3>");
+            echo("<table border='1'>");
+            echo("<tr>
+            <td>fname</td>
+            <td>lname</td>
+            <td>age</td>
+            <td>salary</td>
+            <td>address</td>
+            </tr>");
+            while($row=$fetchedData->fetch_assoc())
+            {
+               echo("<tr>");
+                echo "<td>".$row["fname"]."</td>";
+                echo "<td>".$row["lname"]."</td>";
+                echo "<td>".$row["age"]."</td>";
+                echo "<td>".$row["salary"]."</td>";
+                echo "<td>".$row["address"]."</td>";
+                echo("</tr>");
             }
+            echo("</table>");
         }
 else{
     echo("<br/> No user data found. </br>");
 }
 
-        $dbobj -> closecon($conobj);
+        $dbobj->closecon($conobj);
 }
+
+
 
 
 ?>
